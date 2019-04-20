3e7a70c124924517dce70bfebcf9335ae5012f13
Borislav Stanimirov <b.stanimirov@abv.bg>
2019-04-20 16:34:42 +0300

    Added box

---
+++ include/yama/box.hpp
@@ -0,0 +1,118 @@
+#pragma once
+
+#include "dim.hpp"
+
+namespace yama
+{
+
+template <size_t D, typename T>
+class boxnt
+{
+public:
+    static const size_t dimension = D;
+    typedef typename dim<D>::template vector_t<T> dim_vector;
+
+    ////////////////////////////////////////////////////////
+    // named constructors
+
+    static boxnt inverted()
+    {
+        boxnt ret;
+
+        ret.min = dim_vector::uniform(std::numeric_limits<T>::max());
+        ret.max = dim_vector::uniform(std::numeric_limits<T>::lowest());;
+
+        return ret;
+    }
+
+    static boxnt min_max(const dim_vector& _min, const dim_vector& _max)
+    {
+        boxnt ret;
+
+        ret.min = _min;
+        ret.max = _max;
+
+        return ret;
+    }
+
+    static boxnt pos_size(const dim_vector& pos, const dim_vector& size)
+    {
+        dim_vector end = pos + size;
+
+        boxnt ret;
+
+        ret.min = yama::min(pos, end);
+        ret.max = yama::max(pos, end);
+
+        return ret;
+    }
+
+    ////////////////////////////////////////////////////////
+    // functions
+
+    bool is_inside(const dim_vector& point) const
+    {
+        for(size_t i=0; i<dimension; ++i)
+        {
+            if (point.at(i) < min.at(i) || point.at(i) >= max.at(i))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool intersects(const boxnt& other) const
+    {
+        for(size_t i=0; i<dimension; ++i)
+        {
+            if (min.at(i) >= other.max.at(i))
+                return false;
+
+            if (max.at(i) < other.min.at(i))
+                return false;
+        }
+
+        return true;
+    }
+
+    dim_vector size() const
+    {
+        return max - min;
+    }
+
+    void add_point(const dim_vector& point)
+    {
+        min = yama::min(min, point);
+        max = yama::max(max, point);
+    }
+
+    bool is_valid() const
+    {
+        for(size_t i=0; i<dimension; ++i)
+        {
+            if (max.at(i) < min.at(i)) return false;
+        }
+
+        return true;
+    }
+
+    dim_vector min;
+    dim_vector max;
+};
+
+template <size_t D, typename T>
+boxnt<D, T> intersection(const boxnt<D, T>& a, const boxnt<D, T>& b)
+{
+    boxnt<D, T> ret;
+
+    YAMA_ASSERT_WARN(a.intersects(b), "can't find intersection of non-intersecting boxes");
+
+    ret.min = max(a.min, b.min);
+    ret.max = min(a.max, b.max);
+
+    return ret;
+}
+
+} // namespace yama
