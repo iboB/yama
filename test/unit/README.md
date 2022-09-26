ba9b3510f09b8d3f38ca56aa370285f8fcafd554
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-09-26 20:15:05 +0300

    ALIAS CMake target yama::yama. Latest doctest for tests

--- test/unit/README.md
+++
@@ -1,3 +0,0 @@
-## Yama Unit Tests
-
-* Using a copy of [doctest](https://github.com/onqtam/doctest)
--- test/unit/main.cpp
+++
@@ -1,9 +0,0 @@
-// Yama
-// Copyright (c) 2016-2021 Borislav Stanimirov
-//
-// Distributed under the MIT Software License
-// See accompanying file LICENSE.txt or copy at
-// https://opensource.org/licenses/MIT
-//
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest.h"
